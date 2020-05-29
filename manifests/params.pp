2097078831587f362f1384a0cb9463b996f863be
alfonpd <alfonpd@gmail.com>
2020-05-29 10:03:05 +0200

    Fastchedule optional
    Use -1 for not defined variable

--- manifests/params.pp
+++ manifests/params.pp
@@ -162,7 +162,7 @@ class slurm::params {
   $enforcepartlimits       = 'ALL'
   $epilog                  = ''
   $epilogslurmctld         = ''
-  $fastschedule            = undef           # in [ 0, 1, 2]
+  $fastschedule            = -1           # in [ 0, 1, 2], -1 for ignore
   $grestypes               = []          # list of generic resources to be managed
   $healthcheckinterval     = 30
   $healthchecknodestate    = 'ANY'       # in ['ALLOC', 'ANY','CYCLE','IDLE','MIXED']
